5385075523f1f8ab189271afe283cb688ad2ebab
Hitesh Bhargav <47861535+hiteshcodes@users.noreply.github.com>
2022-05-15 13:31:32 +0530

    GA test1 initial commit

---
+++ gtag.js
@@ -0,0 +1,6 @@
+const GA_MEASUREMENT_ID = process.env.GA_MEASUREMENT_ID
+window.dataLayer = window.dataLayer || [];
+function gtag() { window.dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', GA_MEASUREMENT_ID);
