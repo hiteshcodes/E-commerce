f3525bec4042638f1749b6ca707ea3b8209f14e8
Hitesh Bhargav <47861535+hiteshcodes@users.noreply.github.com>
2022-05-15 16:09:16 +0530

    GA test1

--- gtag.js
+++ gtag.js
@@ -1,5 +1,4 @@
-import config from "./config.json"
-const GA_MEASUREMENT_ID = config.GA_MEASUREMENT_ID
+const GA_MEASUREMENT_ID = "G-DZDK7934TE"
 window.dataLayer = window.dataLayer || [];
 function gtag() { window.dataLayer.push(arguments); }
 gtag('js', new Date());
